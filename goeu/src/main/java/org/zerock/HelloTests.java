0519467305a1b6af273ab4aab7ddd6c440d51d63
goeu <goeu1113@gmail.com>
2019-10-18 19:01:49 +0900

    commit

---
+++ goeu/src/main/java/org/zerock/HelloTests.java
@@ -0,0 +1,18 @@
+package org.zerock;
+
+import org.junit.Test;
+
+/**
+ * HelloTests
+ */
+public class HelloTests {
+
+    @Test
+    public void test1() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
---
+++ sb1/src/test/java/com/zerock/sb1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerock.sb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
